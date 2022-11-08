653413095fc591e17f404ae5192b5ddffc509d97
unknown <preansaha@gmail.com>
2022-11-08 12:38:34 +0600

    uri and client set

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const cors = require('cors');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 // middlewares
@@ -15,6 +15,9 @@ console.log(process.env.DB_USER);
 console.log(process.env.DB_PASSWORD);
 
 
+const uri = "mongodb+srv://process.env.DB_USER:process.env.DB_PASSWORD@cluster0.ul9g96g.mongodb.net/?retryWrites=true&w=majority";
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 app.get('/', (req, res) => {
