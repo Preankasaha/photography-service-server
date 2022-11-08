6164ba8f1581116bdfdd5db9bc4e11bf5dbefe0f
unknown <preansaha@gmail.com>
2022-11-08 12:30:56 +0600

    basic set up

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('hello from photo artisan server')
+})
+app.listen(port, () => {
+    console.log(`listening to port ${port}`);
+})
