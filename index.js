c7f0f029c6545eefaeec8826b1e4dc7f2264c1f0
unknown <preansaha@gmail.com>
2022-11-08 12:34:21 +0600

    middleware added

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const cors = require('cors');
 
-
 const app = express();
 const port = process.env.PORT || 5000;
+// middlewares
+app.use(cors());
+app.use(express.json());
 
 
 
