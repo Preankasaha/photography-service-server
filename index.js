c4aa061b9e1babe00b18aca95c975f73d908ab1a
unknown <preansaha@gmail.com>
2022-11-08 16:54:02 +0600

    get api for service

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
-// middlewares
+
+// middle wares
 app.use(cors());
 app.use(express.json());
-
-
-
 
 console.log(process.env.DB_USER);
 console.log(process.env.DB_PASSWORD);
 
 
-const uri = "mongodb+srv://process.env.DB_USER:process.env.DB_PASSWORD@cluster0.ul9g96g.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fwkvzgi.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+
+
+    try {
+        const serviceCollection = client.db('photographyDb').collection('services');
+
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error)
+
 
 
 app.get('/', (req, res) => {
